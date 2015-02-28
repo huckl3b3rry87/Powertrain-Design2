e153f23102f5adb76883dcfb2c8f318c97c33424
huckl3b3rry87 <rescreen@live.com>
2015-02-28 02:44:07 -0500

    DIRECT and Emissions First

--- Bi_Level_Optimization/branches/DIRECT_DP_version_4.0_______02_23_2015/NOTES.m
+++ Bi_Level_Optimization/branches/DIRECT_DP_version_4.0_______02_23_2015/NOTES.m
@@ -1,6 +1,2 @@
 % New for this Release
-Added a fail for the infeasible shifting in the final simulation
-
-Added smoothed data and messed with direction of matirices.. seems much slower:/
-
-INstead of interpolating now that the grid is so small, maybe I could do nearest
+Fixed issue with smoothed engine data implementation in DP
---
+++ Bi_Level_Optimization/tags/DIRECT_DP_version_4.0_______02_23_2015/NOTES.m
@@ -0,0 +1,4 @@
+% New for this Release
+Fixed issue with smoothed engine data implementation in DP
+
+First release that has the DIRECT and emissions
